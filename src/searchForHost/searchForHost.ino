c147f9b070382d911a8cc5c56d7fb5b074908767
ClémentRenazeau <clement.renazeau@decasoft.fr>
2020-03-06 11:36:39 +0100

    Starting new project on 'search for plug and auto ip detection' subject

---
+++ src/searchForHost/searchForHost.ino
@@ -0,0 +1,17 @@
+/**
+*
+**/
+
+
+#define SECRET_SSID "Bbox-FF996433" // to be replaced by your router SSID
+#define SECRET_PASS "34D476DC271ED15A1E165FFAF241AD"
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
