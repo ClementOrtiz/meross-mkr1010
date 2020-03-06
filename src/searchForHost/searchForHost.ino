bb3081a2ff771dd1314079fa6e2fd740761a53fb
ClémentRenazeau <clement.renazeau@decasoft.fr>
2020-03-06 11:43:42 +0100

    Base of new Wifi project

--- src/searchForHost/searchForHost.ino
+++ src/searchForHost/searchForHost.ino
@@ -2,16 +2,142 @@
 *
 **/
 
+#include <SPI.h>
+#include <WiFiNINA.h>
 
 #define SECRET_SSID "Bbox-FF996433" // to be replaced by your router SSID
 #define SECRET_PASS "34D476DC271ED15A1E165FFAF241AD"
 
-void setup() {
-  // put your setup code here, to run once:
+WiFiClient myClient;
+
+char ssid[] = SECRET_SSID;
+char pass[] = SECRET_PASS;
+int status = WL_IDLE_STATUS;
+
+
+// PIN
+#define BUTTON_PIN 0                // Button pin number
+#define BUTTON_POWER_PIN 6          // Button power pin number => ensure that "BUTTON_PIN" receive 3.3v instead of 5v that it's not recommended
+
+
+// GLOBAL VARIABLES
+int compteur; //check the connection
+int buttonState = LOW;
+bool onOff = false;
+
+
+
+void setup()
+{
+  //Initialize serial and wait for port to open:
+  Serial.begin(9600);
+  pinMode(BUTTON_PIN,INPUT);
+  pinMode(BUTTON_POWER_PIN,OUTPUT);
+
+  while (!Serial)
+  {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // check for the WiFi module:
+  if (WiFi.status() == WL_NO_MODULE)
+  {
+    Serial.println("Communication with WiFi module failed!");
+    // don't continue
+    while (true);
+  }
+
+  String fv = WiFi.firmwareVersion();
+  if (fv < WIFI_FIRMWARE_LATEST_VERSION)
+  {
+    Serial.println("Please upgrade the firmware");
+  }
+
+  // attempt to connect to Wifi network:
+  while (status != WL_CONNECTED)
+  {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network:
+    status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+
+  // you're connected now, so print out the data:
+  Serial.print("You're connected to the network");
+  printCurrentNet();
+  printWifiData();
+
+  // Once every thing is done, power up the button
+  digitalWrite( BUTTON_POWER_PIN, HIGH );
+}
+
+
+void loop() {
+  buttonState = digitalRead(BUTTON_PIN); // the electrical state of the pin '0' is read.
+
+  if(buttonState == HIGH) // == true
+  {
+
+  }
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
 
+void printWifiData()
+{
+  // print your board's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print your MAC address:
+  byte mac[6];
+  WiFi.macAddress(mac);
+  Serial.print("MAC address: ");
+  printMacAddress(mac);
+  delay(1000);
+}
+
+void printCurrentNet()
+{
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print the MAC address of the router you're attached to:
+  byte bssid[6];
+  WiFi.BSSID(bssid);
+  Serial.print("BSSID: ");
+  printMacAddress(bssid);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.println(rssi);
+
+  // print the encryption type:
+  byte encryption = WiFi.encryptionType();
+  Serial.print("Encryption Type:");
+  Serial.println(encryption, HEX);
+  Serial.println();
+}
+
+void printMacAddress(byte mac[])
+{
+  for (int i = 5; i >= 0; i--)
+  {
+    if (mac[i] < 16)
+    {
+      Serial.print("0");
+    }
+    Serial.print(mac[i], HEX);
+    if (i > 0)
+    {
+      Serial.print(":");
+    }
+  }
+  Serial.println();
 }
